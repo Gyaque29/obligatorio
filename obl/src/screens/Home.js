7255b9f9ea82f00a6b1bf30ede2226d78c81acd6
gonzalo <gonzaloyaque29@gmail.com>
2025-06-26 11:25:21 -0300

    Commit 8 Instale fuentes

--- obl/src/screens/Home.js
+++ obl/src/screens/Home.js
@@ -1,9 +1,16 @@
 import { View, Text } from "react-native"
+import { useFonts } from "expo-font"
 
 export const Home = () => {
+    let [fontsLoaded] = useFonts({
+        Dayna: require('../font/static/DynaPuff_Condensed-Bold.ttf'),
+    });
+
+    if (!fontsLoaded) return null
+
     return (
         <View>
-            <Text>PANTALLA INICIO</Text>
+            <Text style={{ fontFamily: "Dayna" }}>pantalla</Text>
         </View>
     )
 }
