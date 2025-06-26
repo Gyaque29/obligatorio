00ae5ddfbfa54ac8c9973a7da3efa9a72ab4343d
gonzalo <gonzaloyaque29@gmail.com>
2025-06-26 09:24:40 -0300

    Commit 3 - Url y Key supabase

--- obl/src/database/database.js
+++ obl/src/database/database.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = "";
-const supabaseKey = "";
+const supabaseUrl = "https://ozsrlwajhgayduuhxkec.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96c3Jsd2FqaGdheWR1dWh4a2VjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk2ODUxODYsImV4cCI6MjA2NTI2MTE4Nn0.fXL-Fgg1XB8BpaaXDHy1Qo-owvJygN1KCbu8BNGrfcw";
 
 const supabase = createClient(supabaseUrl,supabaseKey);
 
