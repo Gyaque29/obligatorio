28c0f38651d8b75e6b5959b5045cc104be6733f2
gonzalo <gonzaloyaque29@gmail.com>
2025-07-02 14:59:33 -0300

    Commit 22 Voy creando la vista de viajes, con componentes hijos para no tener tanto codigo (formulario de nombre y fechas)

---
+++ obl/src/screens/FormViaje.js
@@ -0,0 +1,86 @@
+import { Calendar } from "./Calendar";
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import { useState } from "react";
+import { View, Text, TextInput, Pressable } from "react-native";
+import { styles } from "../styles/styles";
+
+export const FormViaje = ({
+    nombre, setNombre,
+    fechaInicio, setFechaInicio,
+    fechaFin, setFechaFin
+
+}) => {
+
+    //ESTADOS PARA MANEJAR CALENDARIO 
+    const [mostrarCalendario, setMostrarCalendario] = useState(false);
+    const [mostrarCalendario2, setMostrarCalendario2] = useState(false);
+
+    return (
+        <View style={styles.containerformViajes}>
+            <Text
+                style={[{ fontFamily: "Lilita_One" },
+                styles.texttitleseccion]}>
+                Crea un nuevo viaje !
+            </Text>
+
+            <FontAwesome6
+                style={styles.iconos}
+                name="plane-departure"
+                size={24} color="black"
+            />
+
+            <TextInput
+                style={styles.input}
+                placeholder="Ingrese un nombre"
+                value={nombre}
+                onChangeText={setNombre}
+            />
+
+            <View style={{ padding: 20 }}>
+                {/* BOTON QUE MUESTRA LA FECHA INICIO SELECCIONADA */}
+                <Pressable
+
+                    onPress={() => setMostrarCalendario(!mostrarCalendario)}
+                    style={styles.btnFechas}
+                >
+                    <Text style={{ fontFamily: "Lilita_One", fontSize: 16, color: 'white' }}>
+                        Fecha Inicio: {fechaInicio ? fechaInicio.toLocaleDateString() : 'Seleccionar'}
+                    </Text>
+                </Pressable>
+
+                {/* MOSTRAMOS EL CALENDARIO SOLO SI SE MUESTRA EN EL BOTON */}
+                {mostrarCalendario && (
+                    <Calendar
+                        date={fechaInicio}
+                        onSelect={(fecha) => {
+                            setFechaInicio(fecha);
+                            setMostrarCalendario(false); // SE OCULTA LUEGO DE ELEGIR
+                        }}
+                    />
+                )}
+
+                {/* BOTON QUE MUESTRA LA FECHA FIN SELECCIONADA */}
+                <Pressable
+                    onPress={() => setMostrarCalendario2(!mostrarCalendario2)}
+                    style={styles.btnFechas}
+                >
+                    <Text style={{ fontFamily: "Lilita_One", fontSize: 16, color: 'white' }}>
+                        Fecha Fin: {fechaFin ? fechaFin.toLocaleDateString() : 'Seleccionar'}
+                    </Text>
+                </Pressable>
+
+                {/* MOSTRAMOS EL CALENDARIO SOLO SI SE MUESTRA EN EL BOTON */}
+                {mostrarCalendario2 && (
+                    <Calendar
+                        date={fechaInicio}
+                        onSelect={(fecha) => {
+                            setFechaFin(fecha);
+                            setMostrarCalendario2(false); // SE OCULTA LUEGO DE ELEGIR
+                        }}
+                    />
+                )}
+            </View>
+        </View>
+
+    )
+}
--- obl/src/screens/Viajes.js
+++ obl/src/screens/Viajes.js
@@ -2,20 +2,19 @@ import { View, Text, TextInput, Pressable, ScrollView } from "react-native";
 import { useFonts } from "expo-font";
 import { styles } from "../styles/styles";
 import { Costos } from "./Costos";
-import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
-import { Calendar } from "./Calendar";
+
+
 import { useState } from "react";
+import { FormViaje } from "./FormViaje";
 
 export const Viajes = () => {
 
-    //ESTADOS PARA MENJAR LAS FECHAS DE INICIO Y FIN Y CALENDARIO
-    const [fechaInicio, setFechaInicio] = useState(null);
-    const [fechaFin, setFechaFin] = useState(null);
-    const [mostrarCalendario, setMostrarCalendario] = useState(false);
-    const [mostrarCalendario2, setMostrarCalendario2] = useState(false);
-
     //ESTADO PARA MANEJAR NOMBRE VIAJE
     const [nombre, setNombre] = useState('');
+
+    //ESTADOS PARA MENJAR LAS FECHAS DE INICIO Y FIN
+    const [fechaInicio, setFechaInicio] = useState(null);
+    const [fechaFin, setFechaFin] = useState(null);
 
     //FUENTES CARGADAS
     let [fontsLoaded] = useFonts({
@@ -29,76 +28,21 @@ export const Viajes = () => {
 
     if (!fontsLoaded) return null
 
-
     return (
         <View>
             <ScrollView>
-                <View style={styles.containerformViajes}>
-                    <Text
-                        style={[{ fontFamily: "Lilita_One" },
-                        styles.texttitleseccion]}>
-                        Crea un nuevo viaje !
-                    </Text>
+                
+                <FormViaje 
+                    nombre={nombre}
+                    setNombre={setNombre}
+                    fechaInicio={fechaInicio}
+                    setFechaInicio={setFechaInicio}
+                    fechaFin={fechaFin}
+                    setFechaFin={setFechaFin}
+                />
 
-                    <FontAwesome6
-                        style={styles.iconos}
-                        name="plane-departure"
-                        size={24} color="black"
-                    />
+                
 
-                    <TextInput
-                        style={styles.input}
-                        placeholder="Ingrese un nombre"
-                        value={nombre}
-                        onChangeText={setNombre}
-                    />
-
-                    <View style={{ padding: 20 }}>
-                        {/* BOTON QUE MUESTRA LA FECHA INICIO SELECCIONADA */}
-                        <Pressable
-
-                            onPress={() => setMostrarCalendario(!mostrarCalendario)}
-                            style={styles.btnFechas}
-                        >
-                            <Text style={{ fontFamily: "Lilita_One", fontSize: 16, color: 'white' }}>
-                                Fecha Inicio: {fechaInicio ? fechaInicio.toLocaleDateString() : 'Seleccionar'}
-                            </Text>
-                        </Pressable>
-
-                        {/* MOSTRAMOS EL CALENDARIO SOLO SI SE MUESTRA EN EL BOTON */}
-                        {mostrarCalendario && (
-                            <Calendar
-                                date={fechaInicio}
-                                onSelect={(fecha) => {
-                                    setFechaInicio(fecha);
-                                    setMostrarCalendario(false); // SE OCULTA LUEGO DE ELEGIR
-                                }}
-                            />
-                        )}
-
-                        {/* BOTON QUE MUESTRA LA FECHA FIN SELECCIONADA */}
-                        <Pressable
-                            onPress={() => setMostrarCalendario2(!mostrarCalendario2)}
-                            style={styles.btnFechas}
-                        >
-                            <Text style={{ fontFamily: "Lilita_One", fontSize: 16, color: 'white' }}>
-                                Fecha Fin: {fechaFin ? fechaFin.toLocaleDateString() : 'Seleccionar'}
-                            </Text>
-                        </Pressable>
-
-                        {/* MOSTRAMOS EL CALENDARIO SOLO SI SE MUESTRA EN EL BOTON */}
-                        {mostrarCalendario2 && (
-                            <Calendar
-                                date={fechaInicio}
-                                onSelect={(fecha) => {
-                                    setFechaFin(fecha);
-                                    setMostrarCalendario2(false); // SE OCULTA LUEGO DE ELEGIR
-                                }}
-                            />
-                        )}
-                    </View>
-                </View>   
-                        
                 <Pressable
                     style={styles.btnAgregar}
                 >
