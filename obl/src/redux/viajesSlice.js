5f002ce7597de3201c43362bfa66d73a0e262b96
gonzalo <gonzaloyaque29@gmail.com>
2025-07-02 21:23:46 -0300

    Commit 25 corrigo que no se agrege la misma persona a redux

--- obl/src/redux/viajesSlice.js
+++ obl/src/redux/viajesSlice.js
@@ -1,17 +1,20 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-
 export const viajesSlice = createSlice({
     name: 'viajes',
     initialState: {
         integrantes: [],
-        
     },
 
     reducers: {
         addPersona: (state, action) => {
             const persona = action.payload;
-            state.integrantes.push(persona);
+            
+            const existe = state.integrantes.find(i => i.id == persona.id)
+            
+            if(!existe){
+                 state.integrantes.push(persona);
+            }                    
         },
 
         delPersona: (state, action) => {
