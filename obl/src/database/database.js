d1ed14f5430dcbb8041bcf15bb01020bf7ac491a
gonzalo <gonzaloyaque29@gmail.com>
2025-07-08 11:57:42 -0300

    Commit 30

--- obl/src/database/database.js
+++ obl/src/database/database.js
@@ -1,6 +1,4 @@
 import { createClient } from "@supabase/supabase-js"
-import { Costos } from "../screens/Costos";
-
 const supabaseUrl = "https://ozsrlwajhgayduuhxkec.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96c3Jsd2FqaGdheWR1dWh4a2VjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk2ODUxODYsImV4cCI6MjA2NTI2MTE4Nn0.fXL-Fgg1XB8BpaaXDHy1Qo-owvJygN1KCbu8BNGrfcw";
 
